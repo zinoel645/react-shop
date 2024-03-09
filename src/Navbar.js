72a0ea8774e0df2c36ccf1d3a421aced7c5a004b
Thành Long <longthanh937@gmail.com>
2024-03-09 17:08:07 +0700

    done

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,10 +1,33 @@
 import { NavLink } from "react-router-dom";
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 import { AppContext } from "./AppContext.js";
+import Button from "./Button.js";
 
 export default function Navbar(props) {
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
   const app = useContext(AppContext);
   const cartCount = app.getCartCount();
+
+  useEffect(() => {
+    const prefersDark = window.matchMedia(
+      "(prefers-color-scheme: dark)"
+    ).matches;
+    if (prefersDark) {
+      setIsDarkTheme(true);
+    }
+  }, []);
+
+  useEffect(() => {
+    if(isDarkTheme) {
+      document.body.classList.add("dark");
+    } else {
+      document.body.classList.remove("dark");
+    }
+  }, [isDarkTheme])
+
+  function handleThemeClick() {
+    setIsDarkTheme(!isDarkTheme);
+  }
 
   return (
     <nav className="navbar">
@@ -12,6 +35,9 @@ export default function Navbar(props) {
         ZStore
       </NavLink>
       <ul>
+        <li className="nav-item">
+          <Button className="theme-switcher" onClick={handleThemeClick}>{isDarkTheme ? "Dark" : "Light"}</Button>
+        </li>
         <li className="nav-item">
           <NavLink
             className={({ isActive }) => (isActive ? "active" : "")}
